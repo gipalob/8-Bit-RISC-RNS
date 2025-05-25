643ae4f44104957fb33b0a176e9ac4fd61bcc382
bgipalo <136844686+bgipalo@users.noreply.github.com>
2025-05-25 15:33:34 -0400

    added files for iverlog sim

--- 8-bit-RISC-Processor/processor_top.v
+++ 8-bit-RISC-Processor/processor_top.v
@@ -19,10 +19,32 @@ module processor_top (clk, reset, prog_ctr, op1_rd, op2_rd);
         op2_rd = op2_rd_data;
     end
 
-	instr_and_data_mem  mem1(clk, prog_ctr, instr_mem_out, data_rd_addr, data_wr_addr, datamem_rd_data, datamem_wr_data, store_to_mem);
+	instr_and_data_mem  mem1(
+		clk, 
+		prog_ctr, instr_mem_out, data_rd_addr, data_wr_addr, 
+		datamem_rd_data, datamem_wr_data, 
+		store_to_mem
+	);
 
-	processor_core proc1(clk,reset,op1_rd_data,op2_rd_data, instr_mem_out,op1_addr, op2_addr,prog_ctr, store_to_mem, reg_wr_en, data_rd_addr, data_wr_addr, datamem_rd_data, datamem_wr_data, operation_result, destination_reg_addr );
+	processor_core proc1(
+		clk, reset,
+		op1_rd_data, op2_rd_data, 
+		instr_mem_out,
+		op1_addr, op2_addr,
+		prog_ctr, 
+		store_to_mem, reg_wr_en, 
+		data_rd_addr, data_wr_addr, 
+		datamem_rd_data, datamem_wr_data, 
+		operation_result, destination_reg_addr 
+	);
 
-	register_file regfile1(clk, reset, datamem_wr_data, op1_rd_data, op2_rd_data, op1_addr, op2_addr, destination_reg_addr, reg_wr_en);
+	register_file regfile1(
+		clk, reset, 
+		datamem_wr_data, 
+		op1_rd_data, op2_rd_data, 
+		op1_addr, op2_addr, 
+		destination_reg_addr, 
+		reg_wr_en
+	);
 
 endmodule
